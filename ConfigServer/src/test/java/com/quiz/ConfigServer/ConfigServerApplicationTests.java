def6d365e656aacd1d54c18fe5a91ff2433742b6
Chandrkant-majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-03-15 21:46:24 +0530

    Add files via upload
    config

---
+++ ConfigServer/src/test/java/com/quiz/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
